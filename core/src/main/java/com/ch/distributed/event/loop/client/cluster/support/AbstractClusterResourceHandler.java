42c15154b7682a44a856b0be1a910247d03a373c
阔哥 <liqiang@gram-tech.com>
2025-11-30 12:23:34 +0800

    feat(client): 实现集群资源处理器基础框架
    - 添加基于Zookeeper的注册服务支持- 集成InjvmStickyLoadBalance负载均衡策略
    - 实现节点变化监听与日志记录功能
    - 创建FastfailClusterResourceHandler继承抽象类
    - 补充CompletableFuture类型的响应处理逻辑
    - 引入必要的依赖组件和工具类

--- core/src/main/java/com/ch/distributed/event/loop/client/cluster/support/AbstractClusterResourceHandler.java
+++ core/src/main/java/com/ch/distributed/event/loop/client/cluster/support/AbstractClusterResourceHandler.java
@@ -3,17 +3,32 @@ package com.ch.distributed.event.loop.client.cluster.support;
 import com.ch.distributed.event.loop.client.ResourceHandler;
 import com.ch.distributed.event.loop.client.ResourceHandlerRequest;
 import com.ch.distributed.event.loop.client.loadbalance.LoadBalance;
+import com.ch.distributed.event.loop.client.loadbalance.sticky.InjvmStickyLoadBalance;
 import com.ch.distributed.event.loop.common.Node;
 import com.ch.distributed.event.loop.component.Component;
+import com.ch.distributed.event.loop.registry.RegistryService;
+import com.ch.distributed.event.loop.registry.zookeeper.ZookeeperRegistryServiceFactory;
+import org.slf4j.Logger;
 
+import java.util.Collections;
 import java.util.List;
 
 public abstract class AbstractClusterResourceHandler implements ClusterResourceHandler, Component {
     private volatile LoadBalance loadBalance;
+    private volatile RegistryService registryService;
+    private volatile List<Node> nodes;
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(AbstractClusterResourceHandler.class);
 
     @Override
     public void start() {
-        loadBalance = null;
+        // fixme: 这里先用InjvmStickyLoadBalance，后续基于SPI扩展
+        loadBalance = new InjvmStickyLoadBalance();
+        // fixme: 这里先用ZookeeperRegistryService，后续基于SPI扩展、以及Map参数的填充
+        registryService = new ZookeeperRegistryServiceFactory().createRegistryService(Collections.emptyMap());
+        registryService.subscribe(nodes -> {
+            LOGGER.info("收到节点变化通知，原节点 {}，新节点{}", this.nodes, nodes);
+            this.nodes = nodes;
+        });
     }
 
     protected final ResourceHandler doSelect(final ResourceHandlerRequest resourceHandlerRequest, final List<Node> nodes) {
--- core/src/main/java/com/ch/distributed/event/loop/client/cluster/support/FastfailClusterResourceHandler.java
+++ core/src/main/java/com/ch/distributed/event/loop/client/cluster/support/FastfailClusterResourceHandler.java
@@ -1,4 +1,15 @@
 package com.ch.distributed.event.loop.client.cluster.support;
 
-public class FastfailClusterResourceHandler {
+import com.ch.distributed.event.loop.client.ResourceHandlerRequest;
+import com.ch.distributed.event.loop.client.ResourceHandlerResponse;
+
+import java.util.concurrent.CompletableFuture;
+
+public class FastfailClusterResourceHandler extends AbstractClusterResourceHandler{
+    @Override
+    public <R> ResourceHandlerResponse<CompletableFuture<R>> handle(ResourceHandlerRequest resourceHandlerRequest) {
+        final ResourceHandlerResponse<CompletableFuture<ResourceHandlerResponse>> response = doSelect(resourceHandlerRequest)
+                .handle(resourceHandlerRequest);
+        return null;
+    }
 }
