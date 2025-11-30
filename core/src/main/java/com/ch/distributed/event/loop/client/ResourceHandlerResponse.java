d622b9918546e295e75df452d4058e417068d4b9
阔哥 <liqiang@gram-tech.com>
2025-11-30 21:23:52 +0800

    feat(core): 添加对泛型数据返回的支持
    - 在ResourceHandlerResponse接口中添加泛型支持注释
    -为data()方法预留泛型实现空间
    -为后续泛型功能开发提供基础结构

--- core/src/main/java/com/ch/distributed/event/loop/client/ResourceHandlerResponse.java
+++ core/src/main/java/com/ch/distributed/event/loop/client/ResourceHandlerResponse.java
@@ -3,6 +3,7 @@ package com.ch.distributed.event.loop.client;
 public interface ResourceHandlerResponse {
     boolean isSuccess();
 
+    // todo: 支持泛型
     String data();
 
     Exception exception();
